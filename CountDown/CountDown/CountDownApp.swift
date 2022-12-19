bb2f9b8d76ce1ae514f587d419a25baf956ce48f
NorteTryeno <wellkor@yandex.ru>
2022-12-19 13:06:12 +0400

    rep init

---
+++ CountDown/CountDown/CountDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDownApp.swift
+//  CountDown
+//
+//  Created by Денис Дрозд on 19/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CountDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
